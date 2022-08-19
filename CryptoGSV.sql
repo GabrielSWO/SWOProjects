19a5f23fc5fcd1dbb49c05ba5cc94e066930271e
GabrielSWO <104128615+GabrielSWO@users.noreply.github.com>
2022-08-19 12:41:31 +0200

    SQL Projects

---
+++ CryptoGSV.sql
@@ -0,0 +1,8 @@
+-- CRYPTO PROJECT - GABRIEL SANTORUN
+SELECT date,
+    ROUND(AVG(money_in), 2) 
+	AS avg_money_in,
+    ROUND(AVG(money_out), 2) 
+	AS avg_money_out
+FROM transactions
+GROUP BY date;
---
+++ RESTAURANTNY-GSV.sql
@@ -0,0 +1,11 @@
+-- RESTAURANT PROJECT - GABRIEL SANTORUN
+
+SELECT review, 
+ CASE
+  WHEN review > 4.5 THEN 'Extraordinary'
+  WHEN review  > 4 THEN 'Escelent'
+  WHEN review > 3 THEN 'Good'
+  WHEN review  > 2 THEN 'Fair'
+  ELSE 'Poor'
+ END AS "Rating"
+FROM nomnom;
