ae7e60bdef2bc157f490be142574c98a8d646267
GabrielSWO <104128615+GabrielSWO@users.noreply.github.com>
2022-08-22 08:39:20 +0200

    Update script.js

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -2,5 +2,30 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+// Write your code
 
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10)
+};
+
+let compareGuesses = (humanGuess, computerGuess, targetNumber) => {
+
+    let humanPoint = Math.abs(humanGuess - targetNumber);
+    let computerPoint = Math.abs(computerGuess - targetNumber);
+
+    if (humanPoint <= computerPoint) {
+      return true; 
+    } else {
+      return false;
+    }
+  }
+
+const updateScore = (win) => {
+    if (win === 'human') {
+      humanScore += 1;
+    } else if (win === 'computer') {
+      computerScore += 1;
+    }
+  }
+
+  console.log(updateScore('human'));
